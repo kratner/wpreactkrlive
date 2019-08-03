1c83cc896c8ced99f899542da4f4dca10b390f91
Keith Ratner <keith.ratner@gmail.com>
2019-08-02 19:03:16 -0700

    Updates

---
+++ precache-manifest.0c2ad5e81bcd8fa18dee367b20829267.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "362e78c92922b51baaa329bd0f0e23e0",
+    "url": "/wpreactkrlive/index.html"
+  },
+  {
+    "revision": "c6b1f1dc4313a6507f71",
+    "url": "/wpreactkrlive/static/css/main.6e2ebc01.chunk.css"
+  },
+  {
+    "revision": "bca943ab24bdfcd15706",
+    "url": "/wpreactkrlive/static/js/2.f0927397.chunk.js"
+  },
+  {
+    "revision": "c6b1f1dc4313a6507f71",
+    "url": "/wpreactkrlive/static/js/main.45c882c4.chunk.js"
+  },
+  {
+    "revision": "a51601b795aecba68960",
+    "url": "/wpreactkrlive/static/js/runtime~main.4bf3e526.js"
+  },
+  {
+    "revision": "738f05c5ce8af56231e20c5b2eb1de94",
+    "url": "/wpreactkrlive/static/media/Optima-Regular.738f05c5.eot"
+  },
+  {
+    "revision": "8794d246e57e4b306d371c800970308c",
+    "url": "/wpreactkrlive/static/media/Optima-Regular.8794d246.woff2"
+  },
+  {
+    "revision": "a1e2fdad2ee9bc14c0e733255a055754",
+    "url": "/wpreactkrlive/static/media/Optima-Regular.a1e2fdad.ttf"
+  },
+  {
+    "revision": "adc80e91191506fe2bb3bf42f1bbfed2",
+    "url": "/wpreactkrlive/static/media/Optima-Regular.adc80e91.woff"
+  },
+  {
+    "revision": "b797415807f13c7ff03880453e09dca1",
+    "url": "/wpreactkrlive/static/media/Optima-Regular.b7974158.svg"
+  },
+  {
+    "revision": "19915a17d112119b5ca2797b1871cb2b",
+    "url": "/wpreactkrlive/static/media/icomoon.19915a17.woff"
+  },
+  {
+    "revision": "5cc6cbf34a93eec15edbdda13972f7f0",
+    "url": "/wpreactkrlive/static/media/icomoon.5cc6cbf3.eot"
+  },
+  {
+    "revision": "7083b8c447fec0ed84d30356bd7a9e50",
+    "url": "/wpreactkrlive/static/media/icomoon.7083b8c4.svg"
+  },
+  {
+    "revision": "a7d1d9c6ae449f123e0dfe2011c2f10a",
+    "url": "/wpreactkrlive/static/media/icomoon.a7d1d9c6.ttf"
+  }
+]);
